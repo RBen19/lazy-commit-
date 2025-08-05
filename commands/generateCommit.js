c065f333592e5d7c7131228b07ffd5d226997bda
RBen19 <bonmerciel@gmail.com>
2025-08-05 05:05:30 +0000

    refactor: update commands/generateCommit.js

--- commands/generateCommit.js
+++ commands/generateCommit.js
@@ -2,11 +2,20 @@ const { execSync } = require('child_process');
 const inquirer = require('inquirer');
 const isGitRepository = require('../utils/isGitRepo');
 const generateCommitMessage = require('../utils/generateMessage');
+const getPushCommand = require('../utils/initiatePush');
 const fs = require('fs');
 module.exports = {
   command: 'generate-commit',
   aliases: ['gc'],
   desc: 'Generate a conventional commit message',
+  builder: (yargs) => {
+      yargs.option('push', {
+        alias: '-p',
+        describe: 'Push the commit immediately after creation',
+        type: 'boolean',
+        default: false,
+      });
+    },
   handler: async () => {
     console.log('\x1b[34m%s\x1b[0m', 'Generating a conventional commit...');
 
@@ -16,6 +25,18 @@ module.exports = {
     }
 
     try {
+         const pushCommand = getPushCommand;
+      if (!pushCommand && (argv.push || await inquirer.prompt([{
+        type: 'confirm',
+        name: 'confirm',
+        message: 'Aucun remote n\'a été détecté. Le push ne sera pas possible. Continuer ?',
+        default: true
+      }]).then(answers => answers.confirm))) {
+        // Si l'utilisateur a forcé le push ou souhaite continuer sans remote, on l'avertit
+        if(argv.push) {
+          console.log('\x1b[33m%s\x1b[0m', 'Attention : Aucun remote détecté, la commande --push sera ignorée.');
+        }
+      }
       const status = execSync('git status --porcelain').toString().trim();
       if (!status) {
         console.log('\x1b[33m%s\x1b[0m', 'No files to commit. Please stage some files first.');
@@ -31,6 +52,7 @@ module.exports = {
     return match ? match[1] : null;
   })
   .filter(file => file); // garde tous les fichiers listés
+   let committedFiles = 0;
       for (const file of files) {
         const suggestedMessage = generateCommitMessage(file);
 
@@ -48,10 +70,40 @@ module.exports = {
           execSync(`git add "${file}"`);
           execSync(`git commit -m "${suggestedMessage}"`);
           console.log('\x1b[32m%s\x1b[0m', `Successfully committed "${file}".`);
+            committedFiles++;
         } else {
           console.log('\x1b[33m%s\x1b[0m', `Skipping commit for "${file}".`);
         }
       }
+       
+
+
+           if (committedFiles > 0) {
+          if (pushCommand) {
+            let shouldPush = argv.push;
+            if (!shouldPush) {
+              const pushAnswers = await inquirer.prompt([
+                {
+                  type: 'confirm',
+                  name: 'shouldPush',
+                  message: `Voulez-vous pousser les commits vers le dépôt distant avec la commande "${pushCommand}" ?`,
+                  default: false,
+                },
+              ]);
+              shouldPush = pushAnswers.shouldPush;
+            }
+
+            if (shouldPush) {
+              console.log('\x1b[34m%s\x1b[0m', 'Push en cours...');
+              execSync(pushCommand);
+              console.log('\x1b[32m%s\x1b[0m', 'Push réussi.');
+            } else {
+              console.log('\x1b[33m%s\x1b[0m', 'Push ignoré. Les commits sont sauvegardés localement.');
+            }
+          } else {
+            console.log('\x1b[33m%s\x1b[0m', 'Aucun dépôt distant (remote) n\'a été détecté. Le push est ignoré.');
+          }
+        }
 
       console.log('\x1b[32m%s\x1b[0m', 'Git repository detected. Ready to generate a commit!');
     } catch (error) {
